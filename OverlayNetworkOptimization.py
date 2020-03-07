c5fbef84c3166d068c7b5169a64fae14c7319a4b
Tyler Malmberg <tmalmber@cord.edu>
2020-03-06 22:48:12 -0600

    oiuh

--- OverlayNetworkOptimization.py
+++ OverlayNetworkOptimization.py
@@ -71,6 +71,20 @@ def plotChart(best,avg):
 
 
 #########################################################
+# TOURNAMENT METHOD                                     #
+#########################################################
+
+def tournament(fighter1, fighter2):
+    if fighter1.getFitness() > fighter2.getFitness():
+        winner = fighter1
+    elif fighter1.getFitness() < fighter2.getFitness():
+        winner = fighter2
+    else:
+        winner = fighter1
+
+    return winner
+
+#########################################################
 # INDIVIDUAL CLASS                                      #
 # Represents an individual in a population              #             
 #########################################################
@@ -101,26 +115,35 @@ class Individual(object):
     def create_chrom(cls):
         global chromLength
         global BLANK_CHROMOSOME
-        chromosome = BLANK_CHROMOSOME
-        for i in range(0, chromLength):
-            chromosome[i] = int(cls.mutate_gene())
-        return chromosome
+        return [cls.mutate_gene() for _ in range(chromLength)]
 
-    # TODO
     def mate(self, partner):
-        chromeLen = len(self)
-        child1_chrome = []
-        child2_chrome = []
-        crossPt1 = random.randint(0, chromeLen - 1)
-        #crossPt2 = random.randint(0, chromeLen - 1)
+        global chromLength
+        chromeLen = chromLength
+        prob = random.randint(0, 100)
 
-        child1_chrome = np.hstack((self.chromosome[0:crossPt1], partner.chromosome[crossPt1:]))
-        child2_chrome = np.hstack((partner.chromosome[0:crossPt1], self.chromosome[crossPt1:]))
+        if prob <= 70:
+            child1_chrome = []
+            child2_chrome = []
+            crossPt1 = random.randint(0, chromeLen - 1)
+            crossPt2 = random.randint(0, chromeLen - 1)
 
-        child1 = Individual(child1_chrome)
-        child2 = Individual(child2_chrome)
+            child1_chrome = np.hstack((self.chromosome[0:crossPt1], partner.chromosome[crossPt1:]))
+            child2_chrome = np.hstack((partner.chromosome[0:crossPt1], self.chromosome[crossPt1:]))
 
-        return child1, child2
+            child1_chrome = np.hstack((child1_chrome[0:crossPt2], child2_chrome[crossPt2:]))
+            child2_chrome = np.hstack((child2_chrome[0:crossPt2], child1_chrome[crossPt2:]))
+
+            child1 = Individual(child1_chrome)
+            child2 = Individual(child2_chrome)
+            
+            # if prob <= 1:
+            #     child1_chrome = Individual.create_chrom
+            #     child1
+            return child1, child2
+        else:
+            return self, partner
+
 
     def calc_fitness(self):
         global FULL_NETWORK_COST
@@ -168,14 +191,17 @@ def main():
         # RUNNING GA
         # ... to be implemented
         average = 0
-        best = 0
+
+        # Calculate Best Fitness
         for i in range(0, popSize):
             num = evaluateBestFitness(population[i], best)
             if num > best:
                 best = num
         bestM[generation] = best
         
+        # Calculate Avg Fitness
         fitness_total = 0
+        currentFitness = 0
         for i in range(0, popSize):  
             currentFitness = population[i].getFitness()
             fitness_total = fitness_total + currentFitness
@@ -184,6 +210,25 @@ def main():
         print("Generation: ", generation, " Average fitness: ", average)
         #print("Generation: ", generation, " Best fitness: ", best)
         averageM[generation] = average
+
+        # Create New Population
+        new_population = []
+        while (len(new_population) <= popSize):
+            fighter1 = population[random.randint(0, popSize - 1)]
+            fighter2 = population[random.randint(0, popSize - 1)]
+
+            mate1 = tournament(fighter1, fighter2)
+
+            fighter1 = population[random.randint(0, popSize - 1)]
+            fighter2 = population[random.randint(0, popSize - 1)]
+
+            mate2 = tournament(fighter1, fighter2)
+
+            child1, child2 = mate1.mate(mate2)
+
+            new_population.extend([child1, child2])
+
+        population = new_population
         generation = generation + 1
 
     print("best fitness: ", best)   
@@ -191,9 +236,25 @@ def main():
     
     plotChart(bestM, averageM)
 
+
+#########################################################
+# TESTING METHOD                                        #
+#########################################################
+
+def test():
+    chrome = Individual.create_chrom()
+    chrome2 = Individual.create_chrom()
+
+    peep1 = Individual(chrome)
+    peep2 = Individual(chrome2)
+
+    print(chrome)
+    print(chrome2)
+
 #########################################################
 # RUN MAIN                                              #
 #########################################################
 
 if __name__ == '__main__':
     main()
+    #test()
